f3006f24607aba0223a1e79cbc45a790a6a439f2
Salome <sallynanjala254@gmail.com>
2022-10-10 23:44:33 +0300

    Done

--- client/src/Components/Sidebar.js
+++ client/src/Components/Sidebar.js
@@ -3,8 +3,31 @@ import React from 'react';
 const Sidebar = () => {
     return (
         <div>
-            
+      <nav className="navbar d-md-block d-lg-none fixed-top mobile-navbar"><button className="navbar-toggler mr-auto" type="button" data-toggle="collapse" data-target="#mobileNavbarSupportedContent" aria-controls="mobileNavbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="nav-btn">Menu</span></button>
+        <div className="collapse navbar-collapse order-1" id="mobileNavbarSupportedContent">
+          <ul className="navbar-nav mr-auto"></ul>
         </div>
+      </nav>
+      <nav className="sidebar" data-simplebar>
+        <div className="sidebar-header"><a href="#home" data-scroll><img className="img-fluid sidebar-avatar" src="img/flatlay.jpg" alt="Profile avatar"/></a><span className="sidebar-name">Username</span>
+          <p className="sidebar-status">Available for work</p>
+        </div>
+        <div className="sidebar-menu">
+          <ul className="list-unstyled list-menu">
+            <li><a className="nav-link" href="#home-area" data-scroll>Home</a></li>
+            <li><a className="nav-link" href="#about-area" data-scroll>About</a></li>
+            <li><a className="nav-link" href="#services-area" data-scroll>Services</a></li>
+            <li><a className="nav-link" href="#portfolio-area" data-scroll>Portfolio</a></li>
+            <li><a className="nav-link" href="#testimonials-area" data-scroll>Clients</a></li>
+            <li><a className="nav-link" href="#blog-area" data-scroll>Blog</a></li>
+            <li><a className="nav-link" href="#contact-area" data-scroll>Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+    
+    <div className="sections-wrapper">
+    </div>
+    </div>
     );
 }
 
