6ba3d7ddf27d955f299e380b0b75248f51bf6c77
Salome <sallynanjala254@gmail.com>
2022-10-11 14:16:28 +0300

    Modified App.js and Dasboard.js

--- client/src/App.js
+++ client/src/App.js
@@ -23,15 +23,15 @@ function App() {
       <Sidebar/>
       <div class="sections-wrapper">
         <Dashborad/>
-        {/* <Home />
+         <Home />
         <AboutMe/>
         <Services/>
         <HireMe/>
         <Projects/>
-        <Contact/> */}
-        {/* <Footer/> */}
-        {/* <SignUp/>
-        <Login/> */}
+        <Contact/> 
+         <Footer/> 
+         <SignUp/>
+        <Login/> 
 
       
 
