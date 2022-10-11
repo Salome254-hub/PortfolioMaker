eb095f02b17ce5804036af168f225effd6b881cc
denisdelaki <denisdelaki@gmail.com>
2022-10-11 20:06:55 +0300

    Added Validation and association

---
+++ app/models/signup.rb
@@ -0,0 +1,7 @@
+class Signup < ApplicationRecord
+    #association 
+    has_one :user 
+     #validates the email 
+     validates :email, presence: true, uniqueness: true 
+     has_secure_password 
+end
