00de403553f915d02fded99e726622a4ae62fd17
denisdelaki <denisdelaki@gmail.com>
2022-10-11 18:34:20 +0300

    Added the associations and the validations

---
+++ app/models/project.rb
@@ -0,0 +1,9 @@
+class Project < ApplicationRecord
+    #specify the association between the model and other models
+    belongs_to :user 
+    #specify validation requirements for the model
+    validates :project_title, presence: true 
+    validates :project_link, presence: true  
+    validates :image_url, presence: true 
+    
+end
