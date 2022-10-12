3f56f9659a18c9513cfd5589c12ce2a71bae4866
borebrian2021 <bkimutai2021@gmail.com>
2022-10-12 18:54:29 +0300

    Stable version

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,13 @@ Rails.application.routes.draw do
   resources :signups
   resources :projects
   post '/signup', to: 'signups#create'
+  post '/login', to:'sessions#create'
   resources :clients, only: [:index, :create, :show]
   resources :users
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   resources :services, only: [:create, :index, :destroy, :update, :show]
-
   
 end
