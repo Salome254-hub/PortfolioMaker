29825f43f3042b312f8aeab7ade145db47316469
borebrian2021 <bkimutai2021@gmail.com>
2022-11-14 16:41:13 +0300

    Added CORS file

---
+++ config/initializers/cors.rb
@@ -0,0 +1,6 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'https://portfolio-maker-frontend-sycw.vercel.app/'
+      resource '*', headers: :any, methods: [:get, :post,:delete ,:patch]
+    end
+  end
